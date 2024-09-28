beaf03c03103b09163644d8ce2cd6031c59de79f
it <itzik.ben-harush@playermaker.com>
2024-09-28 20:05:31 +0300

    sa

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // Import route handlers
-const addCalories = require('./routes/addCalories');
+const addCaloriesRoute = require('./routes/addCalories');
 const about = require('./routes/about');
 const report = require('./routes/report');
 const index = require('./routes/index');
@@ -52,7 +52,7 @@ mongoose.connect(uri)
   });
   
   // Define routes with their base paths
-  app.use('/addCalories', addCalories); // Route for adding calories
+  app.use('/addCalories', addCaloriesRoute); // Route for adding calories
   app.use('/about', about); // Route for about information
   app.use('/report', report); // Route for generating reports
   app.use('/', index); // Route for the index page
